b774f0edeef8f4935267b70d47bea05a06a6ab3d
Iurii <iuri.ciuneaev@orange.md>
2021-07-03 22:28:38 +0300

    add block for "Buy fonts"

--- 1.test-html-css/assets/app.js
+++ 1.test-html-css/assets/app.js
@@ -3,7 +3,6 @@ const getMenuLinks = document.querySelectorAll('.menu__item');
 
 getMenu.addEventListener('click', (event) => {
   const target = event.target;
-
-  getMenuLinks.forEach(el => el.classList.remove('menu__item--active'))
+  getMenuLinks.forEach(el => el.classList.remove('menu__item--active'));
   target.classList.add('menu__item--active');
 })
